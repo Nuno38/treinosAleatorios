4a3efee58e36b4703690eec1d70e739d2c218020
Elias Nascimento <70302321+Nuno38@users.noreply.github.com>
2024-01-16 12:20:12 -0300

    feat: mehod

--- Js/calculadora.js
+++ Js/calculadora.js
@@ -1,11 +1,28 @@
 /* */
 
-function criaCalculadora{
+function criaCalculadora(){
   return{
+   display:document.querySelector('.display'),
+
+
+
+
 
      inicia(){ 
         alert('oi'); 
-      },  
+      }, 
+
+      cliqueBotoes(){
+        document.addEventListener('click', function(e){
+          const el = e.target;
+
+          if(el.classList.contains('btn-num')){
+            btnParaDisplay();
+          }
+      
+        });
+      },
+
   };
 }
 
