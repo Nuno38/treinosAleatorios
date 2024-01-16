cfef435ee30272992ef915bd64fcc4cf9cf7cd59
Elias Nascimento <70302321+Nuno38@users.noreply.github.com>
2024-01-16 11:26:22 -0300

    feat: created js

--- Js/calculadora.js
+++ Js/calculadora.js
@@ -1,1 +1,13 @@
-/** */
+/* */
+
+function criaCalculadora{
+  return{
+
+     inicia(){ 
+        alert('oi'); 
+      },  
+  };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
