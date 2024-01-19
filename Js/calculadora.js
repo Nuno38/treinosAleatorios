8aa2a8285061224f1a7d6d3b01f9af766bdfe9de
Elias Nascimento <70302321+Nuno38@users.noreply.github.com>
2024-01-19 14:45:02 -0300

    Feat: script of calculator
    Feat(created the script for calculator to by performs the math ): Realizado a criação do script para a calculadora online.

--- Js/calculadora.js
+++ Js/calculadora.js
@@ -1,22 +1,66 @@
-
+ 
 function criaCalculadora() {
   return {
     display: document.querySelector('.display'),
-
-
-    inicia() {
+  
+    inicia(){
       this.cliqueBotoes();
+      this.pressEnter();
+      
     },
+
+    pressEnter(){
+        this.display.addEventListener('keyup', e=>{
+          if (e.keyCode===13){
+            this.mathMake();
+          }
+        });
+    },
+
+    clearDisplay(){
+       this.display.value='';
+    },
+    
+     delOne(){
+        this.display.value = this.display.value.slice(0,-1);
+     },
+
+     mathMake(){
+         let math = this.display.value;
+
+         try{
+           math=eval(math);
+
+           if(!math){
+            alert('Conta inválida');
+            return;
+           }
+           this.display.value=String (math);
+         }catch(e){
+             alert("Conta inválida");
+             return;
+         }
+     },
 
     cliqueBotoes() {
       document.addEventListener('click', function (e) {
         const el = e.target;
-        
+        let letra ="C";
 
         if (el.classList.contains('btn-num')) {
           this.btnParaDisplay(el.innerText);
-
+          
         }
+          if(el.classList.contains('btn-clear')){
+            this.clearDisplay();
+          }
+            
+          if(el.classList.contains('btn-del')){
+            this.delOne();
+          }
+          if(el.classList.contains("btn-eq")){
+              this.mathMake();
+          }
 
       }.bind(this));
     },
@@ -25,6 +69,7 @@ function criaCalculadora() {
     }
   };
 }
+
 
 const calculadora = criaCalculadora();
 calculadora.inicia();
